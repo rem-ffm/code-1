17e5dd46942d43a6df5767d36324cc71c5ddd42d
rem-ffm <105443658+rem-ffm@users.noreply.github.com>
2022-05-13 09:44:31 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 input.onButtonPressed(Button.A, function () {
     keyboard.sendString("a")
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
 })
 input.onButtonPressed(Button.B, function () {
     keyboard.sendString("b")
+    basic.showLeds(`
+        . # # # .
+        . # . . #
+        . # # # .
+        . # . . #
+        . # # # .
+        `)
 })
 keyboard.startKeyboardService()
 basic.showLeds(`
